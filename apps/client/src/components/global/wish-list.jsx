4c5154a72970122d4131c70baa5139626dc1e747
sallah26 <143meski@gmail.com>
2024-05-12 11:04:16 +0300

    done

--- apps/client/src/components/global/wish-list.jsx
+++ apps/client/src/components/global/wish-list.jsx
@@ -8,7 +8,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@app/client/components/ui/sheet";
-import { useCart } from "@app/client/store/cart-store";
+import { useWishlist } from "@app/client/store/wishlist";
 
 import React from "react";
 import ProductCard from "./product-card";
@@ -19,32 +19,33 @@ import Link from "next/link";
 import { cn } from "@app/client/lib/utils";
 import { FaCartArrowDown } from "react-icons/fa";
 import { Heart } from "lucide-react";
+import WishlistProduct from "./wishlist-products";
 
 export default function WishList() {
-  const cart = useCart();
+  const wishlist = useWishlist();
   return (
     <Sheet>
       <SheetTrigger className="relative">
         <span className="relative">
           <Heart />
-          {!!cart.cartProducts.length && (
+          {!!wishlist.wishlistProducts.length && (
             <span className="absolute top-0  right-0 translate-x-2/3 -translate-y-1/2 rounded-full bg-red-600 text-white text-sm flex justify-center items-center  h-[1.2rem] min-w-[1.2rem] px-1">
-              {cart.cartProducts.length}
+              {wishlist.wishlistProducts.length}
             </span>
           )}
         </span>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetTitle>Your wishlist products</SheetTitle>
           <ScrollArea className="h-[calc(100vh_-_8rem)] pe-3">
-            {cart.cartProducts.map((product) => (
-              <CartProduct
+            {wishlist.wishlistProducts.map((product) => (
+              <WishlistProduct
                 key={product}
                 product={product}
               />
             ))}
-            {!cart.cartProducts.length && (
+            {!wishlist.wishlistProducts.length && (
               <div className="w-full h-full justify-center items-center">
                 <h1>No Data</h1>
                 <SheetTrigger
@@ -55,13 +56,6 @@ export default function WishList() {
               </div>
             )}
           </ScrollArea>
-          <div className="">
-            <Link
-              className={cn(buttonVariants(), "w-full")}
-              href={"/checkout"}>
-              Checkout
-            </Link>
-          </div>
         </SheetHeader>
       </SheetContent>
     </Sheet>
