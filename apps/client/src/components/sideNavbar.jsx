cc0d313f1b24f808d97ca5aa815e9b023d1d0ea2
Yonas Adane <yonasadane94@gmail.com>
2024-05-13 04:40:27 +0300

    correcting errors

--- apps/client/src/components/sideNavbar.jsx
+++ apps/client/src/components/sideNavbar.jsx
@@ -9,26 +9,20 @@ import {
     UsersRound,
     } from "lucide-react"
 import { Button } from "./ui/button";
-import { useWindowWidth } from "@react-hook/window-size";
-
 export default function SideNavbar() {
     const [isCollapsed,setIsCollapsed]=useState(false);
-    const onlyWidth=useWindowWidth();
-    var mobileWidth=onlyWidth > 768;
     function toggleSidebar(){
         setIsCollapsed(!isCollapsed)
     }
     return (
         <div className="relative min-w-[80px] border-r px-3 pb-10 pt-24">
-        {!mobileWidth &&
             <div className="absolute right-[-20px] top-7">
                 <Button onClick={toggleSidebar} variant="secondary">
                     <ChevronRight />
                 </Button>
             </div>
-            }
             <Nav
-                isCollapsed={mobileWidth?true:isCollapsed}
+                isCollapsed={isCollapsed}
                 links={[
                     {
                         title: "Dashboard",
